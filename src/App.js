763f953b03ea1c139c5d938dda7f6bc97b8c9039
Will Worth <will.worth@gmail.com>
2019-03-25 18:22:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Header from '../src/components/Header';
+import './App.css';
+import ConferenceList from "../src/components/ConferenceList"
+// import Shortlist from "./components/Shortlist"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App ">
+        <Header/>
+        <ConferenceList className="ConferenceList wrapper"/>
+        {/* <Shortlist/> */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <p>  Frontend Developer Conferences </p>
+       
+      </div>
+    );
+  }
+}
+
+export default Header;
