a5e35208c7d79ec830926e0ff892db1d6f4d0ba4
Will Worth <will.worth@gmail.com>
2019-03-25 19:14:46 +0100

    Updates

---
+++ precache-manifest.975eca1c438696d727aa96f8b070ea87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "723dde2341269b3b2c51",
+    "url": "/confREACTfront/static/css/main.557b3de2.chunk.css"
+  },
+  {
+    "revision": "723dde2341269b3b2c51",
+    "url": "/confREACTfront/static/js/main.613cecf8.chunk.js"
+  },
+  {
+    "revision": "84636d52848ac1b4e464",
+    "url": "/confREACTfront/static/js/runtime~main.0737df75.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/confREACTfront/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "57c7d49693afca5e736b1f3cad06b9c6",
+    "url": "/confREACTfront/index.html"
+  }
+];
